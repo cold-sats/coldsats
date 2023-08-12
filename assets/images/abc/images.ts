3371f7c39725c8be6e84ed9e2784c06d980c51f8
ellismanellisplan <steven@ellis.co>
2023-08-12 10:34:35 -0700

    Auto-generated commit [ci skip]

--- assets/images/abc/images.ts
+++ assets/images/abc/images.ts
@@ -124,16 +124,12 @@ export const images = [
     abcNumber: 534
   },
   {
-    src: 'assets/images/abc/abc30.webp',
-    abcNumber: 6469
-  },
-  {
     src: 'assets/images/abc/abc31.webp',
     abcNumber: 1410
   },
   {
-    src: 'assets/images/abc/abc32.webp',
-    abcNumber: 7537
+    src: 'assets/images/abc/abc30.webp',
+    abcNumber: 6469
   },
   {
     src: 'assets/images/abc/abc33.webp',
@@ -144,60 +140,108 @@ export const images = [
     abcNumber: 9606
   },
   {
-    src: 'assets/images/abc/abc160.webp',
-    abcNumber: 160
+    src: 'assets/images/abc/abc9794.webp',
+    abcNumber: 9794
   },
   {
-    src: 'assets/images/abc/abc217.webp',
-    abcNumber: 217
-  },
-  {
-    src: 'assets/images/abc/abc363.webp',
-    abcNumber: 363
-  },
-  {
-    src: 'assets/images/abc/abc882.webp',
-    abcNumber: 882
-  },
-  {
-    src: 'assets/images/abc/abc939.webp',
-    abcNumber: 939
-  },
-  {
-    src: 'assets/images/abc/abc1296.webp',
-    abcNumber: 1296
-  },
-  {
-    src: 'assets/images/abc/abc1517.webp',
-    abcNumber: 1517
-  },
-  {
-    src: 'assets/images/abc/abc1797.webp',
-    abcNumber: 1797
-  },
-  {
-    src: 'assets/images/abc/abc2198.webp',
-    abcNumber: 2198
-  },
-  {
-    src: 'assets/images/abc/abc2640.webp',
-    abcNumber: 2640
+    src: 'assets/images/abc/abc32.webp',
+    abcNumber: 7537
   },
   {
     src: 'assets/images/abc/abc3560.webp',
     abcNumber: 3560
   },
   {
-    src: 'assets/images/abc/abc3771.webp',
-    abcNumber: 3771
+    src: 'assets/images/abc/abc1517.webp',
+    abcNumber: 1517
   },
+  {
+    src: 'assets/images/abc/abc217.webp',
+    abcNumber: 217
+  },
+  //Dogs
+  {
+    src: 'assets/images/abc/abc2198.webp',
+    abcNumber: 2198
+  },
+  {
+    src: 'assets/images/abc/abc6223.webp',
+    abcNumber: 6223
+  },
+  {
+    src: 'assets/images/abc/abc7730.webp',
+    abcNumber: 7730
+  },
+  //Cats
+  {
+    src: 'assets/images/abc/abc882.webp',
+    abcNumber: 882
+  },
+  {
+    src: 'assets/images/abc/abc1296.webp',
+    abcNumber: 1296
+  },
+  {
+    src: 'assets/images/abc/abc4751.webp',
+    abcNumber: 4751
+  },
+  {
+    src: 'assets/images/abc/abc7009.webp',
+    abcNumber: 7009
+  },
+  {
+    src: 'assets/images/abc/abc160.webp',
+    abcNumber: 160
+  },
+  {
+    src: 'assets/images/abc/abc7850.webp',
+    abcNumber: 7850
+  },
+  {
+    src: 'assets/images/abc/abc8846.webp',
+    abcNumber: 8846
+  },
+  {
+    src: 'assets/images/abc/abc9096.webp',
+    abcNumber: 9096
+  },
+  //Rabbits
   {
     src: 'assets/images/abc/abc3939.webp',
     abcNumber: 3939
   },
   {
-    src: 'assets/images/abc/abc3967.webp',
-    abcNumber: 3967
+    src: 'assets/images/abc/abc4633.webp',
+    abcNumber: 4633
+  },
+  //Fighters
+  {
+    src: 'assets/images/abc/abc2640.webp',
+    abcNumber: 2640
+  },
+  {
+    src: 'assets/images/abc/abc6041.webp',
+    abcNumber: 6041
+  },
+  {
+    src: 'assets/images/abc/abc8116.webp',
+    abcNumber: 8116
+  },
+  {
+    src: 'assets/images/abc/abc363.webp',
+    abcNumber: 363
+  },
+  {
+    src: 'assets/images/abc/abc939.webp',
+    abcNumber: 939
+  },
+  {
+    src: 'assets/images/abc/abc3771.webp',
+    abcNumber: 3771
+  },
+  {
+    src: 'assets/images/abc/abc1797.webp',
+    abcNumber: 1797
   },
   {
     src: 'assets/images/abc/abc3993.webp',
@@ -212,84 +256,25 @@ export const images = [
     abcNumber: 4609
   },
   {
-    src: 'assets/images/abc/abc4633.webp',
-    abcNumber: 4633
-  },
-  {
-    src: 'assets/images/abc/abc4751.webp',
-    abcNumber: 4751
-  },
-  {
-    src: 'assets/images/abc/abc5687.webp',
-    abcNumber: 5687
-  },
-  {
-    src: 'assets/images/abc/abc5709.webp',
-    abcNumber: 5709
-  },
-  {
-    src: 'assets/images/abc/abc6041.webp',
-    abcNumber: 6041
-  },
-  {
-    src: 'assets/images/abc/abc6223.webp',
-    abcNumber: 6223
+    src: 'assets/images/abc/abc8678.webp',
+    abcNumber: 8678
   },
   {
     src: 'assets/images/abc/abc6268.webp',
     abcNumber: 6268
   },
   {
-    src: 'assets/images/abc/abc6363.webp',
-    abcNumber: 6363
-  },
-  {
-    src: 'assets/images/abc/abc6481.webp',
-    abcNumber: 6481
-  },
-  {
-    src: 'assets/images/abc/abc7009.webp',
-    abcNumber: 7009
-  },
-  {
-    src: 'assets/images/abc/abc7058.webp',
-    abcNumber: 7058
-  },
-  {
-    src: 'assets/images/abc/abc7665.webp',
-    abcNumber: 7665
-  },
-  {
-    src: 'assets/images/abc/abc7730.webp',
-    abcNumber: 7730
-  },
-  {
-    src: 'assets/images/abc/abc7850.webp',
-    abcNumber: 7850
-  },
-  {
-    src: 'assets/images/abc/abc8116.webp',
-    abcNumber: 8116
-  },
-  {
-    src: 'assets/images/abc/abc8276.webp',
-    abcNumber: 8276
-  },
-  {
-    src: 'assets/images/abc/abc8678.webp',
-    abcNumber: 8678
-  },
-  {
     src: 'assets/images/abc/abc8793.webp',
     abcNumber: 8793
   },
   {
-    src: 'assets/images/abc/abc8846.webp',
-    abcNumber: 8846
+    src: 'assets/images/abc/abc5687.webp',
+    abcNumber: 5687
   },
+  //My Hero
   {
-    src: 'assets/images/abc/abc9096.webp',
-    abcNumber: 9096
+    src: 'assets/images/abc/abc7058.webp',
+    abcNumber: 7058
   },
   {
     src: 'assets/images/abc/abc9124.webp',
@@ -304,7 +289,28 @@ export const images = [
     abcNumber: 9767
   },
   {
-    src: 'assets/images/abc/abc9794.webp',
-    abcNumber: 9794
+    src: 'assets/images/abc/abc7665.webp',
+    abcNumber: 7665
+  },
+  {
+    src: 'assets/images/abc/abc3967.webp',
+    abcNumber: 3967
+  },
+  //
+  {
+    src: 'assets/images/abc/abc5709.webp',
+    abcNumber: 5709
+  },
+  {
+    src: 'assets/images/abc/abc6481.webp',
+    abcNumber: 6481
+  },
+  {
+    src: 'assets/images/abc/abc6363.webp',
+    abcNumber: 6363
+  },
+  {
+    src: 'assets/images/abc/abc8276.webp',
+    abcNumber: 8276
   },
 ]
